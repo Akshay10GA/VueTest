d9ba098cce73d17d0a3ab91f2bd6725a9367bbfa
Akshay10GA <akshay.aute.7276@gmail.com>
2026-07-01 20:12:58 +0530

    fixes

--- src-capacitor/ios/App/CapacitorRejectShim.swift
+++
@@ -1,9 +0,0 @@
-import Capacitor
-
-extension CAPPluginCall {
-    /// Temporary compatibility shim for plugin code calling `reject(_:)`.
-    /// Routes reject to resolve with an error-shaped payload.
-    func reject(_ message: String) {
-        self.resolve(["success": false, "message": message])
-    }
-}
